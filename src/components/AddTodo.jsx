3fa3cc56577294bc5338d2f391a1722c3f2be082
alvin zhao <yibozhao2018@gmail.com>
2020-10-05 11:50:43 -0700

    components

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddTodo extends Component {
+    render() {
+        return (
+            <div>
+                i am add todo
+            </div>
+        );
+    }
+}
+
+export default AddTodo;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                i am todolist
+            </div>
+        );
+    }
+}
+
+export default TodoList;
