0ec57a07a82a7ec7bcd3f6dce2216db9c050e299
alvin zhao <yibozhao2018@gmail.com>
2020-10-13 16:39:06 -0700

    ctner

--- src/components/containers/AddTodoContainer.js
+++ src/components/containers/AddTodoContainer.js
@@ -1,4 +1,10 @@
 import AddTodo from '../AddTodo'
 import {connect} from 'redux'
+import {setTodoText,addTodo} from '../../actions/index'
+const mSTP = (state)=>({
+    text:state.text
+})
 
-
+const mDTP = dispatch =>({
+    
+})
--- src/components/containers/TodoListContainer.js
+++ src/components/containers/TodoListContainer.js
@@ -1,27 +1,28 @@
-import {connect} from 'redux';
-import TodoList from '../TodoList';
+import { connect } from "react-redux";
+import { toggleTodo } from "../../actions";
+import TodoList from "../TodoList";
 
-const getVisibleTodos =(todos,filter)=>{
-    switch (filter) {
-        case 'all':
-            return todos
-        case 'done':
-            return todos.filter(t=> t.done)
-        case 'active':
-            return todos.filter(t=>!t.done)
-        default:
-            return todos;
-    }
-}
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case "all":
+      return todos;
+    case "done":
+      return todos.filter((t) => t.done);
+    case "active":
+      return todos.filter((t) => !t.done);
+    default:
+      return new Error("unknow filter" + filter);
+  }
+};
 
-const mSTP =(state)=>{
-    return {
-        todos: getVisibleTodos(state.todos, state.filter)
-    }
-}
-const mDTP = (dispatch)=>{
-    return{
-
-    }
-}
-export default connect(mSTP,mDTP)(TodoList)
+const mSTP = (state) => {
+  return {
+    todos: getVisibleTodos(state.todos, state.filter),
+  };
+};
+const mDTP = (dispatch) => {
+  return {
+    toggoleTodo: (id) => dispatch(toggleTodo(id)),
+  };
+};
+export default connect(mSTP, mDTP)(TodoList);
